7cf5e28be0cdb67eeb8f81800eaba7c6932b20d3
Himanshu <himanshuparodiya@gmail.com>
2024-01-31 11:14:37 +0530

    Folder Structure of our page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import SignInForm from "./__auth/forms/SignInForm";
+import SignUpForm from "./__auth/forms/SignUpForm";
+import AuthLayout from "./__auth/AuthLayout";
+import RootLayout from "./__root/RootLayout";
+import { Home } from "./__root/pages";
 
 function App() {
   return (
     <div className="flex h-full">
       <Routes>
-        {/* Public Routes -> without sign in  */}
+        <Route element={<AuthLayout />}>
+          {/* Public Routes -> without sign in  */}
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+        </Route>
 
-        {/* Private Routes -> with sign in  */}
+        <Route element={<RootLayout />}>
+          {/* Private Routes -> with sign in  */}
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </div>
   );
