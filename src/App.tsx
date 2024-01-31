1881ea7a8833c19f35ffff09fcb7039273f667ac
Himanshu <himanshuparodiya@gmail.com>
2024-01-31 11:01:52 +0530

    Initialize react app with tailwind css and react router dom

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex h-full">
+      <Routes>
+        {/* Public Routes -> without sign in  */}
+
+        {/* Private Routes -> with sign in  */}
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
